991b7a9889214d5fd28503731329195761e81f20
malteneu <71597734+malteneu@users.noreply.github.com>
2022-02-14 10:58:40 +0100

    Dinamically loading content

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,65 @@
 const maincontent = document.querySelector('#main-content')
+const navigation_items = document.querySelectorAll('button')
+
+navigation_items.forEach(navigation_item => {
+    navigation_item.addEventListener('click', () => {
+            action(navigation_item)
+    })
+})
 
 loadHome()
 
-/**
- * @param {String} url - address for the HTML to fetch
- * @return {String} the resulting HTML string fragment
- */
 async function fetchHtmlAsText(url) {
-    return await (await fetch(url)).text();
+
+    let page = await fetch("elements/" + url + ".html")
+
+    if(page.status === 200) {
+        return await (page).text();
+    } else {
+        return await (await fetch("elements/404.html")).text();
+    }
 }
 
-// this is your `load_home() function`
-async function loadHome() {
-    maincontent.innerHTML = await fetchHtmlAsText("pages/home.html");
+async function loadElement(element) {
+    maincontent.innerHTML = await fetchHtmlAsText(element)
+    window.scrollTo(0,0)
 }
 
-async function loadCookie() {
-    maincontent.innerHTML = await fetchHtmlAsText("pages/cookies.html");
+function action(navigation_item) {
+
+    switch(navigation_item.getAttribute("id").toLowerCase()) {
+        case '':
+        case '/':
+            loadHome()
+            break;
+        case 'repo':
+            window.open("https://github.com/malteneu/personal_website", "_blank")
+            break;
+        case 'terminal':
+            loadTerminal()
+            break;
+        case 'scroll_up':
+            window.scrollTo({ top: 0, behavior: "smooth" })
+            break;
+        default:
+            loadElement(navigation_item.getAttribute("id"))
+            break;
+    }
 }
+
+function loadHome() {
+    loadElement("home")
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.querySelector('#scroll_up').className = "";
+    } else {
+        document.querySelector('#scroll_up').className = "hidden";
+    }
+}
+
+
+
